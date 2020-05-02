e8301044c2910e8b37aa3d9536df31fe1b42d272
sinkingpoint <colin@quirl.co.nz>
2020-05-02 13:33:34 +0100

    Make cargo vendors project global

--- src/libq/cargo/vendor/bitflags-1.2.1/target/rls/debug/deps/bitflags-381811f4025538c6.d
+++
@@ -1,5 +0,0 @@
-/home/colin/repos/init/src/libq/cargo/vendor/bitflags-1.2.1/target/rls/debug/deps/bitflags-381811f4025538c6.rmeta: src/lib.rs
-
-/home/colin/repos/init/src/libq/cargo/vendor/bitflags-1.2.1/target/rls/debug/deps/bitflags-381811f4025538c6.d: src/lib.rs
-
-src/lib.rs:
--- src/libq/cargo/vendor/bitflags-1.2.1/target/rls/debug/deps/bitflags-9fce4861bbe98b8c.d
+++
@@ -1,5 +0,0 @@
-/home/colin/repos/init/src/libq/cargo/vendor/bitflags-1.2.1/target/rls/debug/deps/bitflags-9fce4861bbe98b8c.rmeta: src/lib.rs
-
-/home/colin/repos/init/src/libq/cargo/vendor/bitflags-1.2.1/target/rls/debug/deps/bitflags-9fce4861bbe98b8c.d: src/lib.rs
-
-src/lib.rs:
