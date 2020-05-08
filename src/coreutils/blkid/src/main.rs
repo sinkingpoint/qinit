3c00285b4183a9b37708a34a4f7ca3c8095e6c01
sinkingpoint <colin@quirl.co.nz>
2020-05-08 18:38:12 +0100

    Support -t auto in mount command, using libq::blkid

---
+++ src/coreutils/blkid/src/main.rs
@@ -0,0 +1,14 @@
+extern crate libq;
+
+use libq::blkid;
+use std::path::PathBuf;
+
+fn main() {
+    let dev = blkid::Device::from_path(PathBuf::from("./cats")).unwrap();
+    if let Some(probe_result) = dev.probe() {
+        println!("{}", probe_result);
+    }
+    else{
+        println!("Failed to probe device");
+    }
+}
--- src/coreutils/mount/src/main.rs
+++ src/coreutils/mount/src/main.rs
@@ -1,17 +1,29 @@
 extern crate clap;
 extern crate nix;
+extern crate libq;
 
 use clap::{Arg,App};
 use nix::mount::{mount,MsFlags};
 use nix::errno::Errno::ENOENT;
+use libq::blkid::Device;
+use std::path::PathBuf;
 
+fn derive_actual_fs_type(dev: &str) -> Option<String> {
+    if let Ok(device) = Device::from_path(PathBuf::from(dev)) {
+        if let Some(probe_result) = device.probe() {
+            return Some(probe_result.get_fs());
+        }
+    }
+
+    return None;
+}
 
 fn main() {
     let args = App::new("mount")
                       .version("0.1")
                       .author("Colin D. <colin@quirl.co.nz>")
                       .about("Mount a filesystem.")
-                      .arg(Arg::with_name("type").short("t").long("type").takes_value(true).required(true))
+                      .arg(Arg::with_name("type").short("t").long("type").takes_value(true))
                       .arg(Arg::with_name("options").short("o").long("options").takes_value(true))
                       .arg(Arg::with_name("device").takes_value(true).index(1).required(true))
                       .arg(Arg::with_name("dir").takes_value(true).index(2).required(true))
@@ -19,7 +31,18 @@ fn main() {
     
     let device = args.value_of("device");
     let dir = args.value_of("dir").unwrap();
-    let mounttype = args.value_of("type");
+    let mut mounttype = args.value_of("type");
+    let actual_type: String;
+    if mounttype == Some("auto") || mounttype == Some("") || mounttype == None {
+        if let Some(fs_type) = derive_actual_fs_type(device.unwrap()) {
+            actual_type = fs_type;
+            mounttype = Some(&actual_type);
+        }
+        else {
+            mounttype = None;
+        }
+    }
+
     match mount::<str, str, str, str>(device, dir, mounttype, MsFlags::empty(), None) {
         Ok(()) => {},
         Err(err) => {
@@ -28,11 +51,13 @@ fn main() {
                     eprintln!("mount {}: {}", dir, "mount point doesn't exist");
                 }
                 else {
-                    eprintln!("{}", err);
+                    eprintln!("mount: Error: {}", err);
                 }
             }
-            eprintln!("{}", err);
-            std::process::exit(1);
+            else {
+                eprintln!("mount: {}", err);
+                std::process::exit(1);
+            }
         }
     };
 }
